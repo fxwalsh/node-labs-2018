a99d8fa63527f44219d3e5da399e77c3fa099f0a
fxwalsh <fxwalsh@wit.ie>
2018-02-05 10:28:37 +0000

    small changes to node-lab1

--- node-lab1/index.js
+++ node-lab1/index.js
@@ -1,14 +1,28 @@
 // Load the http module to create an http server.
 import http from 'http';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import greeting from './greeting.js';
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT
+const port = process.env.PORT;
 // Configure our HTTP server to respond with Hello World to all requests.
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello  World!');
+
+  const default_lang = 'en';
+  const response={
+    lang: default_lang,
+    message: greeting[default_lang],
+  };
+  const lang = req.headers['accept-language'];
+  const message = greeting[lang];
+  if (message){
+    response.lang= lang;
+    response.message = message;
+  }
+  res.writeHead(200, {'Content-Type': 'text/plain',
+                      'Content-Language': response.lang});
+  res.end(response.message);
 });
 
 server.listen(port);
